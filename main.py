d98bf81c859ec9cbcfb8807d9c68dd26d6371505
vgarcialeandro <vgarcialeandro@gmail.com>
2026-05-21 18:48:18 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -12,25 +12,30 @@ def home():
 def health():
     return {"status": "healthy"}
 
+# Modifica el decorador para que acepte AMBAS variantes (con y sin barra)
 @app.get("/webhook", response_class=PlainTextResponse)
+@app.get("/webhook/", response_class=PlainTextResponse)
 def verificar_webhook(
     hub_mode: str = Query(None, alias="hub.mode"),
     hub_challenge: str = Query(None, alias="hub.challenge"),
     hub_verify_token: str = Query(None, alias="hub.verify_token")
 ):
-    # Monitoreo nativo en consola
-    print("--- SOLICITUD ENTRANTE ---")
-    print("MODO EXTRAÍDO POR FASTAPI:", hub_mode)
-    print("TOKEN EXTRAÍDO POR FASTAPI:", hub_verify_token)
-    print("CHALLENGE EXTRAÍDO:", hub_challenge)
+    # Ponemos una condición para ignorar las alertas de las sondas de Azure que vengan vacías
+    if hub_mode is None and hub_verify_token is None:
+        return Response(content="Sonda interna activa", status_code=200)
+
+    print("--- ¡SOLICITUD REAL DETECTADA! ---")
+    print("MODO:", hub_mode)
+    print("TOKEN:", hub_verify_token)
+    print("CHALLENGE:", hub_challenge)
 
     TOKEN_FIJO_DE_PRUEBA = "BotPrueba20260519"
 
     if hub_mode == "subscribe" and hub_verify_token == TOKEN_FIJO_DE_PRUEBA:
-        print("¡ÉXITO TOTAL! Coincidencia perfecta.")
+        print("¡CONEXIÓN EXITOSA CON META!")
         return hub_challenge
 
-    print("¡FALLÓ! Los datos siguen llegando vacíos o no coinciden.")
+    print("¡VERIFICACIÓN FALLIDA! Datos incorrectos.")
     return Response(content="Token invalido", status_code=403)
     
 
